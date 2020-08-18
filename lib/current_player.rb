32d05cca99d0074df1226cfc7f5c38aaf9c1f128
Jarrell Jackson <jdjack3@gmail.com>
2020-08-18 17:51:54 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,13 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
-  counter = 0
-    board.each do |turn|
-    turn != " " || ""
-    counter += 1
+  turn = 0
+    board.each do |move|
+      if move != " " || ""
+        turn += 1
+    end
   end
+  turn
 end
 
 #def current_player
