544c36cc653a640791da4f9ea442000ded68a795
Jarrell Jackson <jdjack3@gmail.com>
2020-08-18 17:39:11 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,9 +3,9 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   counter = 0
   board.each do |turn|
-    "#{turn}"
+    "#{turn} turns have been played"
+    counter += 1
   end
-  counter += 1
 end
 
 #def current_player
